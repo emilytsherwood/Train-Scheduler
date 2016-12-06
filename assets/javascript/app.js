f7a2ccbbdfb13c5c43e5a0ecf82ea80d9263763c
Emily Turner-Sherwood <emilyturnerr@gmail.com>
2016-12-05 20:24:49 -0600

    Firebase working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,39 @@
-app.js
+$(document).ready(function() {
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC8_Nk2_tkeGXYk64BBAlEVvMJc4SM4_Uo",
+    authDomain: "train-schedule-67423.firebaseapp.com",
+    databaseURL: "https://train-schedule-67423.firebaseio.com",
+    storageBucket: "train-schedule-67423.appspot.com",
+    messagingSenderId: "510065519709"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  //Creating variables
+  var trainName = '';
+  var trainDestination = '';
+  var trainFrequency = '';
+  var trainNext = '';
+  var trainMin = '';
+
+  $('#train-form').on('submit', function(){
+  	trainName = $('#name-input').val().trim();
+  	console.log(trainName);
+  	trainDestination = $('#destination-input').val().trim();
+  	trainFrequency = $('#first-input').val().trim();
+  	trainNext = $('#frequency-input').val().trim();
+
+  	database.ref().push({
+  		trainName: trainName,
+  		trainDestination: trainDestination,
+  		trainFrequency: trainFrequency,
+  		trainNext: trainNext,
+  		trainMin: trainMin,
+  	});
+  });
+  return false;
+
+});
